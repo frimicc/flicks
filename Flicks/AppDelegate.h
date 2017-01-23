7fcbb12450098ad14822204ac591b122b1fac85f
Michael Friedman <frimicc@gmail.com>
2017-01-23 10:33:01 -0800

    Initial Commit

---
+++ Flicks/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flicks
+//
+//  Created by  Michael Friedman on 1/23/17.
+//  Copyright © 2017  Michael Friedman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
